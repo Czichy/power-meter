cc2d8320a8135c989a5c837cae2f18b3f2abb337
czichy <christian@czichy.com>
2025-02-11 12:42:19 +0100

    publish reading to mosquitto

--- src/core_loop.rs
+++ src/core_loop.rs
@@ -26,194 +26,6 @@ impl CoreLoop {
         }
     }
 
-    pub fn enter(&self, mqtt_client: rumqttc::Client) -> Result<(), Error> {
-        let port = serialport::new(&self.port, 9_600)
-            .stop_bits(StopBits::One)
-            .parity(Parity::None)
-            .timeout(Duration::from_millis(5000))
-            .open()
-            .expect("Failed to open port");
-
-        // let mut current_ball_position = 1;
-        let mut decoder = sml_rs::transport::Decoder::<Vec<u8>>::new();
-
-        println!("Now listening for SML messages on {}...", self.port);
-
-        for res in port.bytes() {
-            let byte = res?;
-
-            match decoder.push_byte(byte) {
-                Ok(None) => {},
-                Ok(Some(decoded_bytes)) => {
-                    let result = sml_rs::parser::complete::parse(decoded_bytes);
-                    let Ok(sml_file) = result else {
-                        if self.verbose {
-                            println!("Err({:?})", result);
-                        }
-                        continue;
-                    };
-
-                    let reading = MeterReading::parse(sml_file);
-                    let Ok(reading) = reading else {
-                        continue;
-                    };
-                    if self.verbose {
-                        println!("{}", reading.display_compact());
-                    }
-                    // self.database.insert_reading(&reading)?;
-                    if let Some(meter_time) = reading.meter_time {
-                        if let (Some(total_energy_inbound), Some(total_energy_inbound_unit)) = (
-                            &reading.total_energy_inbound,
-                            &reading.total_energy_inbound_unit,
-                        ) {
-                            match mqtt_client.publish(
-                                format!("{MQTT_TOPIC_PREFIX}/meter_time"),
-                                rumqttc::QoS::AtLeastOnce,
-                                false,
-                                format!(
-                                    "{{ \"timestamp\": {meter_time}, \"value\": \
-                                     {total_energy_inbound}, \"unit\" : \
-                                     \"{total_energy_inbound_unit}\" }}",
-                                ),
-                            ) {
-                                Ok(_) => {},
-                                Err(err) => {
-                                    println!("Cannot send to MQTT {err}");
-                                    // continue;
-                                },
-                            };
-                        }
-                        println!("<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>");
-
-                        //     if let (Some(total_energy_outbound),
-                        // Some(total_energy_outbound_unit)) =
-                        // (         &reading.total_energy_outbound,
-                        //         &reading.total_energy_outbound_unit,
-                        //     ) {
-                        //         match mqtt_client.publish(
-                        //
-                        // format!("{MQTT_TOPIC_PREFIX}/meter_time"),
-                        //             rumqttc::QoS::AtLeastOnce,
-                        //             false,
-                        //             format!(
-                        //                 "{{ \"timestamp\": {meter_time},
-                        // \"value\": \
-                        // {total_energy_outbound}, \"unit\" : \
-                        //                  \"{total_energy_outbound_unit}\"
-                        // }}",             ),
-                        //         ) {
-                        //             Ok(_) => {},
-                        //             Err(err) => {
-                        //                 println!("Cannot send to MQTT
-                        // {err}");                 //
-                        // continue;             },
-                        //         };
-                        //     }
-
-                        //     if let (Some(current_net_power),
-                        // Some(current_net_power_unit)) =
-                        //         (&reading.current_net_power,
-                        // &reading.current_net_power_unit)
-                        //     {
-                        //         match mqtt_client.publish(
-                        //
-                        // format!("{MQTT_TOPIC_PREFIX}/meter_time"),
-                        //             rumqttc::QoS::AtMostOnce,
-                        //             false,
-                        //             format!(
-                        //                 "{{ \"timestamp\": {meter_time},
-                        // \"value\": \
-                        // {current_net_power}, \"unit\" :
-                        // \"{current_net_power_unit}\"
-                        // \                  }}",
-                        //             ),
-                        //         ) {
-                        //             Ok(_) => {},
-                        //             Err(err) => {
-                        //                 println!("Cannot send to MQTT
-                        // {err}");                 //
-                        // continue;             },
-                        //         };
-                        //     }
-
-                        //     if let (Some(line_one), Some(line_one_unit)) =
-                        //         (&reading.line_one, &reading.line_one_unit)
-                        //     {
-                        //         match mqtt_client.publish(
-                        //
-                        // format!("{MQTT_TOPIC_PREFIX}/meter_time"),
-                        //             rumqttc::QoS::AtMostOnce,
-                        //             false,
-                        //             format!(
-                        //                 "{{ \"timestamp\": {meter_time},
-                        // \"value\": {line_one}, \
-                        //                  \"unit\" : \"{line_one_unit}\" }}",
-                        //             ),
-                        //         ) {
-                        //             Ok(_) => {},
-                        //             Err(err) => {
-                        //                 println!("Cannot send to MQTT
-                        // {err}");                 //
-                        // continue;             },
-                        //         };
-                        //     }
-                        //     if let (Some(line_two), Some(line_two_unit)) =
-                        //         (&reading.line_two, &reading.line_two_unit)
-                        //     {
-                        //         match mqtt_client.publish(
-                        //
-                        // format!("{MQTT_TOPIC_PREFIX}/meter_time"),
-                        //             rumqttc::QoS::AtMostOnce,
-                        //             false,
-                        //             format!(
-                        //                 "{{ \"timestamp\": {meter_time},
-                        // \"value\": {line_two}, \
-                        //                  \"unit\" : \"{line_two_unit}\" }}",
-                        //             ),
-                        //         ) {
-                        //             Ok(_) => {},
-                        //             Err(err) => {
-                        //                 println!("Cannot send to MQTT
-                        // {err}");                 //
-                        // continue;             },
-                        //         };
-                        //     }
-                        //     if let (Some(line_three), Some(line_three_unit))
-                        // =         (&reading.
-                        // line_three, &reading.line_three_unit)
-                        //     {
-                        //         match mqtt_client.publish(
-                        //
-                        // format!("{MQTT_TOPIC_PREFIX}/meter_time"),
-                        //             rumqttc::QoS::AtMostOnce,
-                        //             false,
-                        //             format!(
-                        //                 "{{ \"timestamp\": {meter_time},
-                        // \"value\": {line_three}, \
-                        //                  \"unit\" : \"{line_three_unit}\"
-                        // }}",             ),
-                        //         ) {
-                        //             Ok(_) => {},
-                        //             Err(err) => {
-                        //                 println!("Cannot send to MQTT
-                        // {err}");                 //
-                        // continue;             },
-                        //         };
-                        //     }
-                    }
-                    self.latest_reading.store(Some(reading));
-                },
-                Err(e) => {
-                    if self.verbose {
-                        println!("Err({:?})", e);
-                    }
-                },
-            }
-        }
-
-        Ok(())
-    }
-
     pub async fn get_data_and_publish(
         &self,
         mqtt_client: &rumqttc::AsyncClient,
@@ -265,19 +77,20 @@ impl CoreLoop {
 
         Ok(())
     }
-
-    pub fn get_latest_reading_cell(&self) -> Arc<AtomicCell<Option<MeterReading>>> {
-        self.latest_reading.clone()
-    }
 }
 
 pub async fn publish_data(
     reading: &MeterReading,
     mqtt_client: &rumqttc::AsyncClient,
 ) -> Result<(), Error> {
-    // let _ = mqtt_client
-    //     .publish("hello/rumqtt", rumqttc::QoS::AtLeastOnce, false, "alive")
-    //     .await;
+    let _ = mqtt_client
+        .publish(
+            "power-meter",
+            rumqttc::QoS::AtLeastOnce,
+            false,
+            "1-HLY03-0207-2343 alive",
+        )
+        .await;
     if let Some(meter_time) = reading.meter_time {
         if let (Some(total_energy_inbound), Some(total_energy_inbound_unit)) = (
             &reading.total_energy_inbound,
